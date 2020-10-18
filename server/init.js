8f8e0e3f343db31c500c1eaa379f5a30af5cc007
root <miroqaan@gmail.com>
2020-10-18 17:45:31 +0900

    server script update - utf encoding added

--- server/init.js
+++ server/init.js
@@ -1,17 +1,22 @@
 var http = require('http');
+var fs = require('fs');
+
 
 function error_404(response){
     response.writeHead(404, {'Content-Type':"text/plain"});
-    response.write("404 Error");
+    response.write("TEST: 404 Error.");
     response.end();
 }
 
 function onRequest(request, response) {
-    if (request.method == 'GET' && request.url == '\n') {
-        response.writeHead(200, {'Content-Type' : 'text/html'});
-        response.createReadStream("../client/userinfo.html").pipe(response);
+    if (request.method == 'GET' && request.url == '/') {
+        response.writeHead(200, {'Content-Type' : 'text/html; charset=utf-8'});
+        fs.createReadStream("../client/userinfo.html").pipe(response);
     } else {
-        error_404(response);
+        response.writeHead(404,{'Content-Type':'text/plain'});
+        response.write(request.method);
+        response.write(request.url);
+	response.end();
     }
 }
 
